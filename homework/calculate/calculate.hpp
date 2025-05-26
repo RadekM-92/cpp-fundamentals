9948d80ffa4315e809690d69342ac7953d21371e
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-05-26 14:20:57 +0200

    Initial implementation of calculate()

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,19 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (0 == command.compare("add")) {
+        return std::to_string(first + second);
+    } else if (0 == command.compare("subtract")) {
+        return std::to_string(first - second);
+    } else if (0 == command.compare("multiply")) {
+        return std::to_string(first * second);
+    } else if (0 == command.compare("divide")) {
+        if (0 != second) {
+            return std::to_string(first / second);
+        } else {
+            return "Division by 0";
+        }
+    } else {
+        return "Invalid data";
+    }
 }
