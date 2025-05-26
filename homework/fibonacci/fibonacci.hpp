ee30a3df3852115f98193684e1b9bba7d0477f54
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-05-26 15:23:50 +0200

    Added fibonacci_iterative() implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result = 0;
+    int result_ = 0;
+    int result__ = 0;
+
+    for (int i = 0; sequence >= i; i++) {
+        if (0 == i) {
+            result = 0;
+
+        } else if (1 == i) {
+            result = 1;
+
+        } else {
+            result__ = result_;
+            result_ = result;
+            result = result_ + result__;
+        }
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
