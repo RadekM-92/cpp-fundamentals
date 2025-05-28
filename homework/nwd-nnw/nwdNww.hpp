3ee3de4d768cbc3d8afc0839e1b76576ad180fd0
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-05-28 22:18:57 +0200

    Added NWD and NWW implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int result = 0;
+    auto lhs_abs = std::abs(lhs);
+    auto rhs_abs = std::abs(rhs);
+
+    for (int i = std::min(lhs_abs, rhs_abs); i > 0; i--) {
+        if ((0 == lhs_abs % i) && (0 == rhs_abs % i)) {
+            result = i;
+            break;
+        }
+    }
+
+    return result;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (0 == lhs || 0 == rhs) {
+        return 0;
+    } else {
+        return (std::abs(lhs * rhs)) / NWD(lhs, rhs);
+    }
 }
