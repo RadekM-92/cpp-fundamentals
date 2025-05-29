b3aeb6014962847c6ea4e4f961676951d7454709
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-05-29 09:47:47 +0200

    Added generator implementation

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> wave;
+    auto sum = 0;
+
+    for (auto i = 0; i < count; i++) {
+        sum = sum + step;
+        wave.push_back(sum);
+    }
+    return wave;
 }
