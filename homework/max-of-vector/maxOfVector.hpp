88a59874c678724d425efad9f6f0b77fea0a49c0
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-05-29 09:28:04 +0200

    Added implementation of maxofVector() fun

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = vec[0];
+
+    for (auto element : vec) {
+        max = std::max(max, element);
+    }
+
+    return max;
 }
